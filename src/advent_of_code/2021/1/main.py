2e6c060192502067dd0c8ded4950820188817c63
Fabian <fabian99.werner@gmail.com>
2021-12-01 13:06:32 +0100

    First Day 2021 ✓

---
+++ src/advent_of_code/2021/1/main.py
@@ -0,0 +1,38 @@
+from advent_of_code.util import open_file, get_File
+
+
+
+
+
+
+def getFirst():
+    i = 0
+    content = open_file('1.txt')
+    for line in content:
+        try:
+            if line > val and line:
+                    i = i + 1
+                    print('Value', line, 'is greater than', val)
+            else:
+                    print('Value', line, 'is less than', val)
+        except:
+            print('First',line)
+        val = line
+    return i
+
+def getSecond():
+    i = 0
+    content = open_file('1.txt')
+    for line in range(3, len(content)):
+        if(content[line] > content[line-3]):
+            i += 1
+    return i
+
+
+if __name__ == '__main__':
+    cookie = input('Enter your cookie: ')
+    get_File('https://adventofcode.com/2021/day/1/input', cookie)
+    print(getFirst())
+    print(getSecond())
+
+
