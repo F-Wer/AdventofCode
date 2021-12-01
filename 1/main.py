e860efbacf35a07dba32ec26b84c6f2ece5b1b3a
Fabian <fabian99.werner@gmail.com>
2021-12-01 10:30:18 +0100

    First Part Done

--- 1/main.py
+++ 1/main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def getFile(url, cookies):
+    cookies = {'session' : cookies}
+    r = requests.get(url, cookies=cookies)
+    with open('1.txt', 'wb') as f:
+        f.write(r.content)
+
+def open_file(file):
+    # Open txt file
+    f = open('1.txt')
+    inp = [int(i) for i in f.read().splitlines()]
+    return inp
+
+def iterateFile():
+    i = 0
+    content = open_file('1.txt')
+    for line in content:
+        try:
+            if line > val and line:
+                    i = i + 1
+                    print('Value', line, 'is greater than', val)
+            else:
+                    print('Value', line, 'is less than', val)
+        except:
+            print('First',line)
+        val = line
+    return i
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # cookie = input('Enter your cookie: ')
+    # getFile('https://adventofcode.com/2021/day/1/input', cookie)
+    print(iterateFile())
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
