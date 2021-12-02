21755d517a6b0cb2d11e33434f81fe6a6fb82011
Fabian <fabian99.werner@gmail.com>
2021-12-02 12:01:20 +0100

    Added a new solution, "inspried" from reddit

--- src/advent_of_code/2021/2/main.py
+++ src/advent_of_code/2021/2/main.py
@@ -22,6 +22,8 @@ def getFirst():
             print('ERROR')
     return forward * horizontal
 
+
+
 def getSecond():
     aim = 0
     horizontal = 0
@@ -47,11 +49,26 @@ def getSecond():
     return  horizontal , depth, aim
 
 
+def getRedditSolution():
+#  nice solution with structural pattern matching
+#  source: https://old.reddit.com/r/adventofcode/comments/r6zd93/2021_day_2_solutions/hmwbtbe/
+    h = d = a = 0
+    for x in open('2.txt'):
+        match x.split():
+            case 'forward', n:
+                h += int(n)
+                d += int(n) * a
+            case 'up', n:
+                a -= int(n)
+            case 'down', n:
+                a += int(n)
+
+    print(h * a, h * d)
+
 
 if __name__ == '__main__':
     cookie = input('Enter your cookie: ')
     get_File('https://adventofcode.com/2021/day/2/input', cookie, '2')
-    print(getFirst())
     result = getSecond()
     print(result)
     print(result[0] * result[1])
